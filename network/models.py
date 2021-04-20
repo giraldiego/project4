505bf569fb3f7af685ce714c3e1fae6314e3d553
Diego Giraldo <giraldiego@gmail.com>
2021-04-19 20:52:37 -0500

    Add User and Post models

--- network/models.py
+++ network/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    follows = models.ManyToManyField("self", related_name="followers", symmetrical=False)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts")
